d35fadd4b32cc96c64d0c4b2c7963febe7009dee
tarkatnare-hash <tarkatnare@gmail.com>
2026-04-08 00:17:33 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Portfolio Loaded");
+
+// Simple smooth scroll
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
+    });
+});
